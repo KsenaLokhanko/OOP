52928c01c1c052a101e4ea2c3362dd35fb097504
KsenaLokhanko <99492970+KsenaLokhanko@users.noreply.github.com>
2022-03-29 11:38:48 +0300

    Add files via upload

---
+++ Lab3.java
@@ -0,0 +1,11 @@
+public class Lab3 {
+    public static void main(String[] args){
+        System.out.println("Hello, world!");
+
+        System.out.println("C3 is " + 1117 % 3 + " so C3 is StringBuilder;");
+        System.out.println("C17 is " + 1117 % 17 + " so C17 task is Відсортувати слова заданого тексту \nза кількістю входжень визначеного символу в них.");
+
+        
+
+    }
+}
